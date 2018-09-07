2cafdc080526959cd4f3f2387d6b4420db8a1a26
kalpesh <kalpeshgupta30@gmail.com>
2018-09-07 21:19:14 +0530

    Phase 2 going on

--- app/src/main/java/com/example/kalpesh/shopkart/MainActivity.java
+++ app/src/main/java/com/example/kalpesh/shopkart/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.kalpesh.shopkart;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -22,7 +26,7 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String BASE_URL = "https://bitnami-39xfosdmxa.appspot.com/get-items" ;
+    public static final String BASE_URL = "https://bitnami-39xfosdmxa.appspot.com/get-items";
     private static RecyclerView recyclerView;
 
     @Override
@@ -30,36 +34,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = (RecyclerView)findViewById(R.id.recycler_view) ;
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
         StringRequest request = new StringRequest(BASE_URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                Log.v("REs",response);
+                Log.v("REs", response);
                 GsonBuilder builder = new GsonBuilder();
                 Gson gson = builder.create();
-                ArrayList<Phone> phones = new ArrayList<Phone>(Arrays.asList(gson.fromJson(response, Phone[].class))) ;
+                ArrayList<Phone> phones = new ArrayList<Phone>(Arrays.asList(gson.fromJson(response, Phone[].class)));
                 renderPhone(phones);
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getBaseContext(),"Couldn't get list from the net",Toast.LENGTH_LONG).show();
+                Toast.makeText(getBaseContext(), "Couldn't get list from the net", Toast.LENGTH_LONG).show();
             }
         });
 
-        RequestQueue queue = Volley.newRequestQueue(this) ;
-        queue.add(request) ;
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
 
 
     }
 
-    public void renderPhone(ArrayList<Phone> phones){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu, menu);
+        return true;
+    }
 
-        ListAdapter adapter = new ListAdapter(getApplicationContext(),phones) ;
-        recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(),2));
+
+    @Override
+    public void onBackPressed(){
+        Intent intent = new Intent(Intent.ACTION_MAIN) ;
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags()
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.op_search:
+                searchActivity();
+                break;
+            default:
+        }
+        return true;
+    }
+
+    public void searchActivity() {
+        Intent intent = new Intent(this,SearchActivity.class);
+        startActivity(intent);
+    }
+
+    public void renderPhone(ArrayList<Phone> phones) {
+
+        ListAdapter adapter = new ListAdapter(getApplicationContext(), phones);
+        recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
         recyclerView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
 
     }
+
 }
--- app/src/main/java/com/example/kalpesh/shopkart/SearchActivity.java
+++ app/src/main/java/com/example/kalpesh/shopkart/SearchActivity.java
@@ -1,13 +1,31 @@
 package com.example.kalpesh.shopkart;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private static EditText manufacturer;
+    private static EditText model;
+    private static EditText min;
+    private static  EditText max ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        manufacturer = findViewById(R.id.et_manufacturer);
+        model = findViewById(R.id.et_model) ;
+        min = findViewById(R.id.et_min) ;
+        max = findViewById(R.id.et_max) ;
+        Intent intent = new Intent(this,MainActivity.class) ;
+        Bundle bundle = new Bundle() ;
+        bundle.putString("Manufacturer",manufacturer.getText().toString());
+        bundle.putString("Model",manufacturer.getText().toString());
+        //bundle.putString("");
+
     }
 }
